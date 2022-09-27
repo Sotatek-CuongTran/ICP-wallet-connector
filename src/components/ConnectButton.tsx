ac61cd899c575eaabf3c4a9620d6c144ef128cfe
William Tran <cuong.tran@sotatek.com>
2022-09-27 23:42:43 +0700

    feat: update code to support ICP

--- src/components/ConnectButton.tsx
+++ src/components/ConnectButton.tsx
@@ -1,21 +1,83 @@
 import { Button, Box, Text } from "@chakra-ui/react";
-import { useEthers, useEtherBalance } from "@usedapp/core";
-import { formatEther } from "@ethersproject/units";
 import Identicon from "./Identicon";
+import { useState } from "react";
+declare const window: any;
 
 type Props = {
   handleOpenModal: any;
 };
 
 export default function ConnectButton({ handleOpenModal }: Props) {
-  const { activateBrowserWallet, account } = useEthers();
-  const etherBalance = useEtherBalance(account);
+  const [currentPrincipalId, setCurrentPrincipalId] = useState("");
+  const [actor, setActor] = useState<any>();
 
-  function handleConnectWallet() {
-    activateBrowserWallet();
+  async function handleConnectWallet() {
+    try {
+      // Add the Sonic mainnet canister to whitelist
+      const sonicCanisterId = "3xwpq-ziaaa-aaaah-qcn4a-cai";
+      const whitelist = [sonicCanisterId];
+
+      // Create an interface factory from a canister's IDL
+      const sonicPartialInterfaceFactory = ({ IDL }: any) => {
+        const TokenInfoExt = IDL.Record({
+          id: IDL.Text,
+          fee: IDL.Nat,
+          decimals: IDL.Nat8,
+          name: IDL.Text,
+          totalSupply: IDL.Nat,
+          symbol: IDL.Text,
+        });
+        const PairInfoExt = IDL.Record({
+          id: IDL.Text,
+          price0CumulativeLast: IDL.Nat,
+          creator: IDL.Principal,
+          reserve0: IDL.Nat,
+          reserve1: IDL.Nat,
+          lptoken: IDL.Text,
+          totalSupply: IDL.Nat,
+          token0: IDL.Text,
+          token1: IDL.Text,
+          price1CumulativeLast: IDL.Nat,
+          kLast: IDL.Nat,
+          blockTimestampLast: IDL.Int,
+        });
+        const SwapInfo = IDL.Record({
+          owner: IDL.Principal,
+          cycles: IDL.Nat,
+          tokens: IDL.Vec(TokenInfoExt),
+          pairs: IDL.Vec(PairInfoExt),
+        });
+        return IDL.Service({
+          getSwapInfo: IDL.Func([], [SwapInfo], ["query"]),
+        });
+      };
+
+      const sonicActor = await window.ic.plug.createActor({
+        canisterId: sonicCanisterId,
+        interfaceFactory: sonicPartialInterfaceFactory,
+      });
+
+      // Request a connection
+      // Will fire onConnectionUpdate on account switch
+      await window?.ic?.plug?.requestConnect({
+        whitelist,
+      });
+
+      setCurrentPrincipalId((window as any).ic.plug.principalId);
+      setActor(sonicActor);
+    } catch (e) {
+      console.log(e);
+    }
+    // activateBrowserWallet();
   }
 
-  return account ? (
+  const handleGetSwapInfo = async () => {
+    // use our actors getSwapInfo method
+    const swapInfo = await actor.getSwapInfo();
+    console.log("Sonic Swap Info: ", swapInfo);
+  };
+
+  return currentPrincipalId ? (
     <Box
       display="flex"
       alignItems="center"
@@ -23,11 +85,6 @@ export default function ConnectButton({ handleOpenModal }: Props) {
       borderRadius="xl"
       py="0"
     >
-      <Box px="3">
-        <Text color="white" fontSize="md">
-          {etherBalance && parseFloat(formatEther(etherBalance)).toFixed(3)} ETH
-        </Text>
-      </Box>
       <Button
         onClick={handleOpenModal}
         bg="gray.800"
@@ -44,13 +101,28 @@ export default function ConnectButton({ handleOpenModal }: Props) {
         height="38px"
       >
         <Text color="white" fontSize="md" fontWeight="medium" mr="2">
-          {account &&
-            `${account.slice(0, 6)}...${account.slice(
-              account.length - 4,
-              account.length
-            )}`}
+          {currentPrincipalId}
         </Text>
         <Identicon />
+      </Button>
+      <Button
+        onClick={handleGetSwapInfo}
+        bg="gray.800"
+        border="1px solid transparent"
+        _hover={{
+          border: "1px",
+          borderStyle: "solid",
+          borderColor: "blue.400",
+          backgroundColor: "gray.700",
+        }}
+        borderRadius="xl"
+        m="1px"
+        px={3}
+        height="38px"
+      >
+        <Text color="white" fontSize="md" fontWeight="medium" mr="2">
+          Handle get swap info
+        </Text>
       </Button>
     </Box>
   ) : (
